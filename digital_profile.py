cc0924bb3ad04740c346a07b9a33dceda41e08ef
413-04-teepakorn <54226@yupparaj.ac.th>
2026-06-25 09:57:37 +0700

    Create digital_profile.py

---
+++ digital_profile.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("My digital profile")
+
+st.header("แนะนำตัว")
+st.write("นายทีปกร ปัญญาวงค์")
+st.write("ชื่อเล่น แบงค์")
+st.write("เลขที่ 4 ม.4/13")
+
+st.header("เป้าหมายที่อยากทำในอนาคต")
+st.write("เป้าหมาย : **อยากเรียนต่อสัตวแพทย์")
+st.write("สิ่งที่อยากเป็นในอนาคต: สัตวแพทย์ (veterinary)")
